63c846544b2ab667fda0fc87c57767b10a519e47
gioudi <sergiopenagos881@gmail.com>
2024-07-22 21:54:26 -0500

    fix-button-component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -11,10 +11,43 @@ export default meta;
 
 type Story = StoryObj<typeof Button>
 
+export const Default: Story = {
+    args:{
+        label: 'Default Button',
+        onClick: ()=> alert('Default Button Clicked')
+    }
+}
+
+export const DefaultLarge: Story = {
+    args:{
+        label: 'Default Large Button',
+        onClick: ()=> alert('Default Button Clicked'),
+        size: 'large',
+    }
+}
+
 export const Primary: Story = {
     args:{
         label: 'Primary Button',
-        onClick: ()=> alert('Primary Button Clicked')
+        onClick: ()=> alert('Primary Button Clicked'),
+        variant: 'primary',
+    }
+}
+
+export const PrimarySmall: Story = {
+    args:{
+        label: 'Primary  Small Button',
+        onClick: ()=> alert('Small Button Clicked'),
+        variant: 'primary',
+        size:'small'
+    }
+}
+
+export const Secondary: Story = {
+    args:{
+        label: 'Secondary Button',
+        onClick: ()=> alert('Secondary Button Clicked'),
+        variant: 'secondary',
     }
 }
 
